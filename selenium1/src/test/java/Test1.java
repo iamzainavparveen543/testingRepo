7d02366bc46b58baf3187e7e45335ad76f36704d
zainav <iamzainavparveen543@gmail.com>
2025-07-12 12:59:25 +0530

    "WedDriverManager added"

--- selenium1/src/test/java/Test1.java
+++ selenium1/src/test/java/Test1.java
@@ -1,9 +1,12 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
     public static void main(String[] args){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\MyLearnings\\chromedriver.exe");
-        ChromeDriver dr = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\MyLearnings\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        WebDriver dr = new ChromeDriver();
         dr.get("https://www.amazon.in/");
 
     }
